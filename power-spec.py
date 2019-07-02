4a80d0f5f91b46b30414fda90b536f68367671e0
João Buschinelli <alexbuschinelli@gmail.com>
2019-07-02 15:33:30 -0400

    New code

--- power-spec.py
+++ power-spec.py
@@ -3,105 +3,63 @@ from numpy.fft import fft, fft2, fftfreq, ifft, ifft2, fftshift, fftn, ifftn, if
 import matplotlib.pyplot as plt
 import random as rand
 
+def power_spec(box,dx,bins):
+    N = int(np.sqrt(np.size(box)))
+    ibox = (dx**2)*fftshift(fft2(box))
+    ibox_abs_sq = np.abs(ibox)**2
+    K = k_matrix(box,dx)
+    unique_K = np.unique(K,return_counts=True)
+    max_k = np.amax(unique_K[0])
+    ps = np.zeros((bins))
+    step = max_k/bins
+    p = 0
+    while p*step < max_k:
+        n_ele = 0
+        for i in range(-N//2,N//2):
+            for j in range(-N//2,N//2):
+                z = np.sqrt(i**2+j**2)/(N*dx)
+                if z < (p+1)*step and z >= p*step :
+                    ps[p] = ps[p] + ibox_abs_sq[i,j]     
+                    n_ele = n_ele + 1         
+        ps[p] = ps[p]/n_ele
+        p = p + 1
+    
+    ps = ps/((N*dx)**2)
+    return ps
 
 
-
-## Take a NxN box in real space, compute its FFT and take its absolute
-## value.
-
-def abs_field(box):
-    fft_box = fftshift(fft2((box)))
-    psi_sqr = (np.abs(fft_box)**2)
-
-    return psi_sqr
-
-
-## Creates a grid containing all points in fourier space, and the values
-## of k^2 = kx^2 + ky^2 corresponding to that point
-
-def k_matrix(box):
-    size = int(m.sqrt(np.size(box)))
-    kx = fftshift(fftfreq(size))
-    ky = fftshift(fftfreq(size))
+def k_matrix(box,dx):
+    size = int(np.sqrt(np.size(box)))
+    kx = fftshift(fftfreq(size,d=dx))
+    ky = fftshift(fftfreq(size,d=dx))
     k = np.zeros((size,size),dtype='float')
     for i in range(0,size):
         for j in range(0,size):
             k[i,j] = np.sqrt(kx[i]**2+ky[j]**2)
     return k
 
-## Outputs all possible values for k in a array, from smallar to larger k's
 
-def unique_k(k):
-    return np.unique(k)
-
-## Computes the 1D power spectrum array, containing P(k), for each value of k
-
-def power_spec(box):
-    k = k_matrix(box)
-    size = int(m.sqrt(np.size(box)))
-    unique_elements = np.unique(k)
-    counts_elements = np.unique(k, return_counts=True)
-    fft_psi_abs = abs_field(box)
-    kx = fftshift(fftfreq(size))
-    ky = fftshift(fftfreq(size))
-    ps = np.zeros(len(unique_elements),dtype='float')
-    
-    for i in range(0,len(unique_elements)):
-        for j in range(0,len(kx)):
-            for n in range(0,len(ky)):
-                if unique_elements[i] == m.sqrt(kx[j]**2 + ky[n]**2):
-                    ps[i] = ps[i] + fft_psi_abs[j,n]
-        ps[i] = ps[i]/counts_elements[1][i]    
-    return ps
-
-## Plots the power spectrum as a function of k.
-
-def plot_ps(box):
-    ps = power_spec(box)
-    k = unique_k(k_matrix(box))
-    plt.plot(k,ps)
-    plt.xlabel('Frequency')
-    plt.ylabel('Power spectrum')
-    plt.show()
-    
-
-############### REVERSE PROCESS #################
-#################################################
-
-        
-
-
-
-def count_dim(ps_size):
-    for i in range(1,1001):
-        if ps_size == count_size(2*i):
-            n = int(2*i)
-            return n
-
-    return 0
-
-
-## This is the main code: it uses as input a given power spectrum and compute some box that would have as power spec
-## the array ps inputed that way
-
-def find_box(ps):
-    size = np.size(ps)
-    N = count_dim(size)
+def find_box(ps,dx,N):
+    bins = len(ps)
     a = np.random.normal(size=(N,N))
     b = np.random.normal(size=(N,N))
     box = a + 1j*b
     shift_box = fftshift(box)
-    K = fftshift(k_matrix(box))
-    k_value = np.unique(K,return_counts=True)    
+    K = k_matrix(box,dx)
+    unique_K = np.unique(K,return_counts=True) 
+    max_k = np.amax(unique_K[0])
+    step = max_k/bins
+    p = 0
+    for i in range(-N//2,N//2):
+        for j in range(-N//2,N//2):
+            n_ele = 0
+            while p*step < max_k:
+                z = np.sqrt(i**2+j**2)/(N*dx)
+                if z < (p+1)*step and z >= p*step:
+                    el = np.random.normal(0,np.sqrt(ps[p]/2),size=(1,1))
+                    box[i,j] = el[0]
+                p = p + 1
 
-    
-    for i in range(0,N):
-        for j in range(0,N):
-            k = np.sqrt(i**2+j**2)/N
-            k_compare = k_value[0] - k
-            loc_k = np.where(k_compare == 0)[0]
-            if len(loc_k) > 0 :
-                box[i,j] = np.sqrt(ps[loc_k[0]]/2)*box[i,j]
     
     for i in range(0,N):
         for j in range(0,N):
@@ -109,20 +67,17 @@ def find_box(ps):
                 box[i,j] = np.conjugate(box[-i,-j])
             else:
                 box[i,j] = np.real(box[i,j])
-            
-    return box
-
-            
+                
     
-
-## Counts the size of the power spectrum array produced by a n x n box
-
-
-def count_size(n):
-    x = fftshift(np.random.normal(size=(n,n)))
-    ps = power_spec(x)
-    ps_size = len(ps)
-    return ps_size
+    return ifft2(box)/(dx**2)
     
+dx = 1
+N = 4
+bins = 4
 
-
+box = np.random.normal(size=(N,N))
+ps = power_spec(box,dx,bins)
+boxf = find_box(ps,dx,N)
+psf = power_spec(boxf,dx,bins)
+plt.plot(make_k(box,dx,bins),ps)
+plt.plot(make_k(box,dx,bins),psf)
